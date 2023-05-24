a09b96bb8bc3baf14980d5844c41cf40800197eb
Nikita Mikhaulov <Sience1999@gmail.com>
2023-05-24 20:34:33 +0200

    plant icons

---
+++ lib/plant_icons.dart
@@ -0,0 +1,3 @@
+library plant_icons;
+
+export 'package:plant_icons/plant_icons_data.dart';
---
+++ test/plant_icons_test.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:plant_icons/plant_icons.dart';
+
+void main() {
+  test('returns IconData', () {
+    expect(PlantIcons.add, IconData);
+  });
+}
